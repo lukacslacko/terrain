970093d8483927cdc0ca437c59403991d632e94a
Laszlo Lukacs <lukacslacko@gmail.com>
2025-04-21 21:57:02 +0200

    more state

--- src/state.rs
+++ src/state.rs
@@ -7,3 +7,34 @@ pub struct ImageHandle(pub Handle<Image>);
 pub struct GameTime {
     pub time: f32,
 }
+
+#[derive(Resource)]
+pub struct MapSize {
+    pub width: usize,
+    pub height: usize,
+}
+
+#[derive(Resource)]
+pub struct MapState {
+    pub height_map: Vec<Vec<f32>>,
+}
+
+impl MapState {
+    pub fn new(map_size: &MapSize) -> Self {
+        MapState {
+            height_map: vec![vec![0.0; map_size.width]; map_size.height],
+        }
+    }
+
+    pub fn update_image(&self, image: &mut Image, time: f32) {
+        for i in 0..256 {
+            for j in 0..256 {
+                let pixel = image.pixel_bytes_mut(UVec3::new(i, j, 0)).unwrap();
+                let shift = ((time / 5.0).fract() * 256.0) as u32;
+                pixel[0] = ((i + shift) % 256) as u8;
+                pixel[1] = ((j + shift) % 256) as u8;
+            }
+        }
+
+    }
+}
--- src/ui.rs
+++ src/ui.rs
@@ -1,21 +1,21 @@
-use crate::state::GameTime;
-use crate::state::ImageHandle;
+use crate::state::*;
 
 use bevy::asset::RenderAssetUsages;
 use bevy::prelude::*;
 use bevy::render::render_resource::Extent3d;
 use bevy::render::render_resource::{TextureDimension, TextureFormat};
 
-pub fn init() {
+pub fn init(width: usize, height: usize) {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_systems(Startup, setup)
-    .add_systems(Update, update_image)
-    .run();
+        .add_plugins(DefaultPlugins)
+        .insert_resource(MapSize { width, height })
+        .add_systems(Startup, setup)
+        .add_systems(Update, update_image)
+        .run();
 }
 
-fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
-    let mut image = Image::new_fill(
+fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>, map_size: Res<MapSize>) {
+    let image = Image::new_fill(
         Extent3d {
             width: 256,
             height: 256,
@@ -27,18 +27,11 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
         RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
     );
 
-    for i in 0..256 {
-        for j in 0..256 {
-            let pixel = image.pixel_bytes_mut(UVec3::new(i, j, 0)).unwrap();
-            pixel[0] = i as u8;
-            pixel[1] = j as u8;
-        }
-    }
-
     let image_handle = images.add(image).clone();
 
     commands.insert_resource(ImageHandle(image_handle.clone()));
-    commands.insert_resource(GameTime{time: 0.0});
+    commands.insert_resource(GameTime { time: 0.0 });
+    commands.insert_resource(MapState::new(&map_size));
 
     commands.spawn(Sprite::from_image(image_handle));
 
@@ -50,16 +43,10 @@ fn update_image(
     mut images: ResMut<Assets<Image>>,
     image_handle: Res<ImageHandle>,
     mut game_time: ResMut<GameTime>,
+    map_state: Res<MapState>,
 ) {
     game_time.time += time.delta_secs();
     let image = images.get_mut(&image_handle.0).unwrap();
 
-    for i in 0..256 {
-        for j in 0..256 {
-            let pixel = image.pixel_bytes_mut(UVec3::new(i, j, 0)).unwrap();
-            let shift = ((game_time.time / 5.0).fract() * 256.0) as u32;
-            pixel[0] = ((i + shift) % 256) as u8;
-            pixel[1] = ((j + shift) % 256) as u8;
-        }
-    }
+    map_state.update_image(image, game_time.time);
 }
