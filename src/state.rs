a611b5e17bbdf8cf82001f215b517441e74981cd
Laszlo Lukacs <lukacslacko@gmail.com>
2025-04-24 23:43:06 +0200

    lakes

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,6 @@
 use crate::terrain::height_map;
 use bevy::prelude::*;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Resource)]
 pub struct ImageHandle(pub Handle<Image>);
@@ -22,18 +22,104 @@ pub struct MapState {
 
 impl MapState {
     pub fn new(width: usize, height: usize) -> Self {
-        let height_map = height_map(width, height);
+        let mut height_map = height_map(width, height);
         let mut points_with_height = Vec::new();
         let mut lake_id: Vec<Vec<usize>> = vec![vec![0; width]; height];
+        let mut actual_lake_id = HashMap::new();
+        let mut overflown_lakes = HashSet::new();
+        actual_lake_id.insert(0, 0);
+        let mut lake_level = HashMap::new();
         let mut next_lake_id = 1;
-        for i in 0..width {
-            for j in 0..height {
-                points_with_height.push((height_map[j][i], j, i));
+        for col in 0..width {
+            for row in 0..height {
+                points_with_height.push((height_map[row][col], row, col));
             }
         }
         points_with_height.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        let is_edge =
+            |row: usize, col: usize| row == 0 || col == 0 || row == height - 1 || col == width - 1;
+        let mut points_handled = 0;
+        for (h, row, col) in points_with_height {
+            points_handled += 1;
+            if points_handled == 10000000 {
+                break;
+            }
+            let neighbor_lake_ids = [
+                (row.saturating_sub(1), col),
+                ((row + 1).min(height - 1), col),
+                (row, col.saturating_sub(1)),
+                (row, (col + 1).min(width - 1)),
+            ]
+            .iter()
+            .filter(|&(r, c)| height_map[*r][*c] < h)
+            .map(|&(r, c)| actual_lake_id[&lake_id[r][c]])
+            .collect::<HashSet<_>>();
+            if is_edge(row, col) {
+                lake_id[row][col] = 0;
+                for lake_id in neighbor_lake_ids.iter() {
+                    if *lake_id == 0 {
+                        continue;
+                    }
+                    overflown_lakes.insert(*lake_id);
+                }
+                continue;
+            }
+            if neighbor_lake_ids.is_empty() {
+                lake_id[row][col] = next_lake_id;
+                println!("Lakes: {}", next_lake_id);
+                actual_lake_id.insert(next_lake_id, next_lake_id);
+                lake_level.insert(next_lake_id, h);
+                next_lake_id += 1;
+                continue;
+            }
+            if neighbor_lake_ids.contains(&0) {
+                // TODO check if there's a non-zero neighbor, too.
+                // In that case, this is where the lake drains, and we
+                // should track the river from here down.
+                for other_lake_id in neighbor_lake_ids.iter() {
+                    if *other_lake_id == 0 {
+                        continue;
+                    }
+                    overflown_lakes.insert(*other_lake_id);
+                }
+                lake_id[row][col] = 0;
+                continue;
+            }
+            let non_overflow_neighbor_lake_ids = neighbor_lake_ids
+                .iter()
+                .filter(|&&x| !overflown_lakes.contains(&x))
+                .cloned()
+                .collect::<HashSet<_>>();
+            if non_overflow_neighbor_lake_ids.is_empty() {
+                lake_id[row][col] = 0;
+                continue;
+            }
+            // This point is part of one ore more lakes.
+            let smallest_lake_id = non_overflow_neighbor_lake_ids
+                .iter()
+                .cloned()
+                .min()
+                .unwrap();
+            lake_id[row][col] = smallest_lake_id;
+            lake_level.insert(smallest_lake_id, h);
+            for other_lake_id in non_overflow_neighbor_lake_ids {
+                if other_lake_id == smallest_lake_id {
+                    continue;
+                }
+                actual_lake_id.insert(other_lake_id, smallest_lake_id);
+            }
+        }
 
         let mut is_water = vec![vec![false; width]; height];
+        for row in 0..height {
+            for col in 0..width {
+                if lake_id[row][col] == 0 {
+                    continue;
+                }
+                is_water[row][col] = true;
+                // height_map[row][col] = lake_level[&actual_lake_id[&lake_id[row][col]]]
+            }
+        }
         let min_height = height_map
             .iter()
             .flatten()
